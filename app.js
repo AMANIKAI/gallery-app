6ed67d5844b6c8a01ae6e03106f799c753bc4087
amani <akai@safaricom.co.ke>
2023-01-19 12:42:36 +0300

    added modules

--- app.js
+++ app.js
@@ -1,10 +1,43 @@
+// Import express package
+const express = require('express');
+
+let indexRouter = require('./routes/index');
+
+// Initialize express
+const app = express();
+
+// Set up a view engine
+app.set('view engine', 'ejs');
+
+
 // Set a static folder
 app.use(express.static('public'));
 
+// Define the index router
+app.use('/', indexRouter);
 
-app.get('/', (req,res)=>{
-   res.render('index');
-})
+//connecting to the db
+const mongoose = require("mongoose");
+
+let DB_NAME = process.env.DB_NAME;
+let DB_USER = process.env.DB_USER;
+let DB_PASS = process.env.DB_PASS;
+let DB_URL = process.env.DB_URL
+let db_url = "mongodb+srv://" + DB_USER + ":" + DB_PASS + "@galleryapp.hdeaftp.mongodb.net/" +DB_NAME;
+console.log(db_url);
+let option = {
+    maxPoolSize:10,
+    family:4
+};
+mongoose.connect(db_url,option);
+let db = mongoose.connection
+db.once("open",() => {
+    console.log("Successful connection to the database");
+});
+
+db.on("error", ()=> {
+    console.log("Connection to DB failed");
+});
 
 
 // Define the port number
---
+++ models/accounts.js
@@ -0,0 +1,7 @@
+let mongoose = require("mongoose");
+
+let accounts = {
+
+
+}
+
